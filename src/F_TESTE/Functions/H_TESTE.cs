0dace0d0094dcd2d60876f35a6a90bceb9fb9c41
rafael45 <rafael450140@gmail.com>
2026-02-18 23:17:45 -0300

    Add Azure Function H_TESTE with QueueTrigger

--- src/F_TESTE/Functions/H_TESTE.cs
+++ src/F_TESTE/Functions/H_TESTE.cs
@@ -1,1 +1,19 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
+namespace F_TESTE.Functions;
+
+public class H_TESTE
+{
+    private readonly ILogger _logger;
+
+    public H_TESTE(ILoggerFactory loggerFactory)
+        => _logger = loggerFactory.CreateLogger<H_TESTE>();
+
+    [Function("H_TESTE")]
+    public void Run(
+        [QueueTrigger("h-teste", Connection = "AzureWebJobsStorage")] string message)
+    {
+        _logger.LogInformation("H_TESTE recebeu: {msg}", message);
+    }
+}
