ee1c076afc5dd21e015421cce71d2368d21be661
rafael45 <rafael450140@gmail.com>
2026-02-18 23:25:05 -0300

    Update I_TESTE.cs

--- src/F_TESTE/Functions/I_TESTE.cs
+++ src/F_TESTE/Functions/I_TESTE.cs
@@ -1,1 +1,19 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
+namespace F_TESTE.Functions;
+
+public class I_TESTE
+{
+    private readonly ILogger _logger;
+
+    public I_TESTE(ILoggerFactory loggerFactory)
+        => _logger = loggerFactory.CreateLogger<H_TESTE>();
+
+    [Function("I_TESTE")]
+    public void Run(
+        [QueueTrigger("iteste", Connection = "AzureWebJobsStorage")] string message)
+    {
+        _logger.LogInformation("I_TESTE recebeu: {msg}", message);
+    }
+}
